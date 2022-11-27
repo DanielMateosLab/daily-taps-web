cf4d5da1a3c7685d1b7e85158711aff6ad52bdd0
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-11-27 17:21:29 +0100

    Remove legacy pages

---
+++ app/head.tsx
@@ -0,0 +1,17 @@
+const Head = async () => (
+  <>
+    <title>Daily Taps</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charSet="UTF-8" />
+    <meta
+      name="description"
+      content="An app to help you keep track of your training sessions."
+    />
+    <meta
+      name="keywords"
+      content="workouts, strength training, lifting, powerlifting, training"
+    />
+  </>
+);
+
+export default Head;
---
+++ app/layout.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from "react";
+
+const AppLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default AppLayout;
