a8d962b54c8cc929e933d2c7c0757339058169d2
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-12-05 16:51:20 +0100

    Update icons size

--- icons/system.tsx
+++ icons/system.tsx
@@ -5,7 +5,7 @@ const SystemIcon = () => (
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="w-6 h-6"
+    className="w-4 h-4"
   >
     <path
       strokeLinecap="round"
