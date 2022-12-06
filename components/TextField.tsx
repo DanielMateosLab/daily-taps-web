3182d719dee171f843da65e85f8935c4813cbcdc
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-12-06 10:29:07 +0100

    Fix issue with invalid: TW modifier

--- components/TextField.tsx
+++ components/TextField.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { RefCallback, useCallback, useRef } from "react";
 import { useField } from "formik";
+import React from "react";
 
 export interface TextFieldProps {
   name: string;
@@ -11,6 +11,8 @@ export interface TextFieldProps {
 
 const TextField: React.FC<TextFieldProps> = (props) => {
   const [field, meta] = useField(props.name);
+  const invalid = !!meta.error && meta.touched;
+  const borderClass = invalid ? "border" : "";
 
   return (
     <div className="flex flex-col gap-1.5 relative">
@@ -18,14 +20,12 @@ const TextField: React.FC<TextFieldProps> = (props) => {
         {props.label}
       </label>
       <input
-        className="px-3 py-2 text-lg text-on-surface-normal bg-surface-2 rounded-lg invalid:border border-red-500"
+        className={`px-3 py-2 text-lg text-on-surface-normal bg-surface-2 rounded-lg border-red-500 ${borderClass}`}
         id={props.name}
         {...field}
         {...props}
       />
-      <div className="h-5 text-sm text-red-500">
-        {meta.error && meta.touched && meta.error}
-      </div>
+      <div className="h-5 text-sm text-red-500">{invalid && meta.error}</div>
     </div>
   );
 };
