e7cc22d3f94d478cb7040162c3d1991bcc3cd5a4
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2023-01-28 14:08:52 +0100

    Switch to useAuthenticator hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,17 @@
-import { Authenticator, Button, Heading, Text } from "@aws-amplify/ui-react"
+import { Button, Heading, Text, useAuthenticator } from "@aws-amplify/ui-react"
 
 export default function Home() {
+  const { user, signOut } = useAuthenticator((context) => [context.user])
+
   return (
-    <div>
-      <Authenticator>
-        {({ signOut, user }) => (
-          <div className="flex flex-col items-center justify-center gap-4 pt-8">
-            <Heading level={1}>Welcome!</Heading>
-            <Text variation="secondary">
-              You are signed in as {user?.attributes?.email}
-            </Text>
-            <Button onClick={signOut} className="w-fit">
-              Sign Out
-            </Button>
-          </div>
-        )}
-      </Authenticator>
+    <div className="flex flex-col items-center justify-center gap-4 pt-8">
+      <Heading level={1}>Welcome!</Heading>
+      <Text variation="secondary">
+        You are signed in as {user?.attributes?.email}
+      </Text>
+      <Button onClick={signOut} className="w-fit">
+        Sign Out
+      </Button>
     </div>
   )
 }
