2c2f050b6f9ff7d5ed3e61003ce88a1bde350d8b
Daniel Mateos <daniel.mat.lab@gmail.com>
2023-02-09 22:23:11 +0100

    Add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
-import "@aws-amplify/ui-react/styles.css"
-import { Amplify } from "aws-amplify"
-import type { AppProps } from "next/app"
-import Head from "next/head"
-import "../styles/globals.css"
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
 
-import awsExports from "../src/aws-exports"
-import { Authenticator } from "@aws-amplify/ui-react"
-import AppAuthenticator from "../src/app-components/AppAuthenticator"
-Amplify.configure({ ...awsExports, ssr: true })
+import awsExports from "../src/aws-exports";
+import { Authenticator, ThemeProvider } from "@aws-amplify/ui-react";
+import AppAuthenticator from "../src/app-components/AppAuthenticator";
+import theme from "../src/theme";
+Amplify.configure({ ...awsExports, ssr: true });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -21,11 +22,13 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Authenticator.Provider>
-        <AppAuthenticator>
-          <Component {...pageProps} />
-        </AppAuthenticator>
-      </Authenticator.Provider>
+      <ThemeProvider theme={theme}>
+        <Authenticator.Provider>
+          <AppAuthenticator>
+            <Component {...pageProps} />
+          </AppAuthenticator>
+        </Authenticator.Provider>
+      </ThemeProvider>
     </>
-  )
+  );
 }
