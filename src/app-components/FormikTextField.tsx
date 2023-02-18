c4639e0833f9b43af5f40ab8fdbda4ebcc623a09
Daniel Mateos <daniel.mat.lab@gmail.com>
2023-02-18 23:45:15 +0100

    Allow custom classNames in FormikTextField

--- src/app-components/FormikTextField.tsx
+++ src/app-components/FormikTextField.tsx
@@ -5,20 +5,24 @@ import { useDateInputFixer } from "../hooks/useDateInputFixer";
 
 type FormikTextFieldProps = Pick<
   TextFieldProps,
-  "label" | "descriptiveText" | "placeholder" | "type"
+  "label" | "descriptiveText" | "placeholder" | "type" | "className"
 > & {
   name: string;
 };
 
 const FormikTextField: FC<FormikTextFieldProps> = (props) => {
   const [field, meta] = useField(props.name);
-  const colorModeFix = useDateInputFixer(props.type);
+  const { css, className: colorModeClass } = useDateInputFixer(props.type);
+  const className = [colorModeClass, props.className].filter(Boolean).join(" ");
 
   return (
     <TextField
       errorMessage={meta.error}
       hasError={meta.touched && !!meta.error}
-      {...{ ...props, ...field, ...colorModeFix }}
+      {...props}
+      {...field}
+      css={css}
+      className={className}
     />
   );
 };
