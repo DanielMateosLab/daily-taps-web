02bfe4d3525d783c656029203631195da7b5ce4d
Daniel Mateos <daniel.mat.lab@gmail.com>
2023-02-19 09:20:15 +0100

    Improve layout across screens

--- src/app-components/AppAuthenticator.tsx
+++ src/app-components/AppAuthenticator.tsx
@@ -4,33 +4,36 @@ import {
   Text,
   useAuthenticator,
   View,
-} from "@aws-amplify/ui-react"
-import { css } from "@emotion/react"
-import { FC, PropsWithChildren } from "react"
+} from "@aws-amplify/ui-react";
+import { FC, PropsWithChildren } from "react";
 
 const AppAuthenticator: FC<PropsWithChildren> = ({ children }) => {
-  const { authStatus } = useAuthenticator((context) => [context.authStatus])
+  const { authStatus } = useAuthenticator((context) => [context.authStatus]);
 
   const renderMap = {
-    configuring: <Authenticator>Loading...</Authenticator>,
+    // The authenticator component has to be rendered to trigger the auth flow
+    configuring: (
+      <View as="main" className="grid min-h-screen place-items-center">
+        <Text>Loading...</Text>
+        <Authenticator className="invisible absolute" />
+      </View>
+    ),
     authenticated: <Authenticator>{children}</Authenticator>,
     unauthenticated: (
-      <main
-        className="flex flex-col items-center gap-4 pt-10"
-        css={css`
-          min-height: 100svh;
-        `}
+      <View
+        as="main"
+        className="flex min-h-screen flex-col items-center gap-4 px-4 pt-10"
       >
         <Heading level={1}>Daily Taps</Heading>
         <Text variation="secondary" className="text-lg">
           Please sign in to continue
         </Text>
         <Authenticator className="mt-4" />
-      </main>
+      </View>
     ),
-  }
+  };
 
-  return <View>{renderMap[authStatus]}</View>
-}
+  return renderMap[authStatus];
+};
 
-export default AppAuthenticator
+export default AppAuthenticator;
